8f8c68d94d3a0b1a59257d871c45bb1d3b8921c2
RomainRDB <rduboullay@gmail.com>
2024-11-29 00:32:44 +0100

    ajustements

--- start-server.mjs
+++ start-server.mjs
@@ -1,5 +1,13 @@
-import { writeFile } from 'fs/promises';
+import { createServer } from 'vite';
 
-const port = process.env.PORT || 10000; // Utilise le port attribué par Render ou 10000 par défaut
-await writeFile('port.txt', port, 'utf8');
-console.log(`Port value written to port.txt: ${port}`);
+const port = process.env.PORT || 10000; // Utiliser le port attribué par Render ou 10000 par défaut
+
+// Créer un serveur Vite
+createServer({
+  server: {
+    port: parseInt(port),  // Prendre en compte le port fourni
+    host: '0.0.0.0',       // Permettre l'accès depuis toutes les interfaces
+  }
+}).listen(port, () => {
+  console.log(`Server is running at http://0.0.0.0:${port}`);
+});
