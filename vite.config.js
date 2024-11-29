438b57ad7fd4f6ee3e259a10580e1b48c6cffb3d
RomainRDB <rduboullay@gmail.com>
2024-11-29 02:20:21 +0100

    last commit

--- vite.config.js
+++ vite.config.js
@@ -10,17 +10,20 @@ export default defineConfig({
             refresh: true,
         }),
     ],
-    server: {
-        host: '0.0.0.0', // écoute sur toutes les interfaces réseau
-        port: process.env.PORT ? parseInt(process.env.PORT) : 5173, // Utiliser le port fourni par Render ou 5173 par défaut
-        strictPort: true, // Si le port n'est pas disponible, arrêter
-    },
     build: {
-        outDir: 'dist', // Assurez-vous que le build est généré dans le dossier dist
+        outDir: 'public/build', // Le dossier de sortie pour les fichiers générés
+        manifest: true, // Crée un manifest.json pour l'intégration Laravel-Vite
         rollupOptions: {
-            input: {
-                main: 'resources/views/index.html', // Assurez-vous que ce chemin est correct
+            // Pour éviter les conflits de nommage et permettre à Laravel de trouver les bons fichiers
+            output: {
+                assetFileNames: 'assets/[name].[hash][extname]',
+                chunkFileNames: 'assets/[name].[hash].js',
+                entryFileNames: 'assets/[name].[hash].js',
             },
         },
+    },
+    server: {
+        host: '0.0.0.0', // Permet d'exposer le serveur localement pour le développement
+        port: 5173, // Port local pour le développement (modifiable selon vos préférences)
     },
 });
