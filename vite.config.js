cc62b775c6494cec55d5cdebbbd148f3ef28e89b
RomainRDB <rduboullay@gmail.com>
2024-11-29 02:47:50 +0100

    last commit

--- vite.config.js
+++ vite.config.js
@@ -1,29 +1,13 @@
-import { defineConfig } from "vite";
-import laravel from "laravel-vite-plugin";
-import vue from "@vitejs/plugin-vue";
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
     plugins: [
         vue(),
         laravel({
-            input: ["resources/js/app.js"],
+            input: ['resources/js/app.js'],
             refresh: true,
         }),
     ],
-    build: {
-        outDir: 'public/build', // Le dossier de sortie pour les fichiers générés
-        manifest: true, // Crée un manifest.json pour l'intégration Laravel-Vite
-        rollupOptions: {
-            // Pour éviter les conflits de nommage et permettre à Laravel de trouver les bons fichiers
-            output: {
-                assetFileNames: 'assets/[name].[hash][extname]',
-                chunkFileNames: 'assets/[name].[hash].js',
-                entryFileNames: 'assets/[name].[hash].js',
-            },
-        },
-    },
-    server: {
-        host: '0.0.0.0', // Permet d'exposer le serveur localement pour le développement
-        port: 5173, // Port local pour le développement (modifiable selon vos préférences)
-    },
 });
